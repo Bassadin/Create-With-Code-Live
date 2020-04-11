a5b5d9d4054d18777db8a3d2315735d65c9e5820
Bastian Hodapp <bastianhodapp@gmail.com>
2020-04-11 23:29:11 +0200

    Apply destroy script to animals

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -3,10 +3,11 @@
 public class DestroyOutOfBounds : MonoBehaviour
 {
     private float topBound = 30f;
+    private float lowerBound = -10f;
 
     void Update()
     {
-        if (transform.position.z > topBound) {
+        if (transform.position.z > topBound || transform.position.z < lowerBound) {
             Destroy(gameObject);
         }
     }
