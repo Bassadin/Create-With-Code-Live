0ffc5522d1a820e632b3997a1e2b86df1f3d3de5
Bastian Hodapp <bastianhodapp@gmail.com>
2020-04-11 22:31:47 +0200

    Player movement boundaries

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float moveSpeed = 5f;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        if (transform.position.x < -10) {
+            horizontalInput = Mathf.Clamp(horizontalInput, 0, 1);
+        }
+
+        if (transform.position.x > 10) {
+            horizontalInput = Mathf.Clamp(horizontalInput, -1, 0);
+        }
+
+        transform.Translate(Vector3.right * horizontalInput * moveSpeed * Time.deltaTime);
+    }
+}
