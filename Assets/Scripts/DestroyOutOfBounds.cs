6993bb96a325fcf1536b9d8577ea1ca342bf5056
Bastian Hodapp <bastianhodapp@gmail.com>
2020-04-11 23:21:24 +0200

    create outofbounds scripts

---
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    private float topBound = 30f;
+
+    void Update()
+    {
+        if (transform.position.z > topBound) {
+            Destroy(gameObject);
+        }
+    }
+}
