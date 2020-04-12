56b29784a47075177c7f490bbebd4fa5f681084c
Bastian Hodapp <bastianhodapp@gmail.com>
2020-04-12 18:24:38 +0200

    Make animals spawn at intervals

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,20 +4,20 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
     public float xSpawnRange = 13f;
+    public float zSpawnOffset = 2f;
 
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            spawnRandomAnimal();
-        }
+    public float startDelay = 1.5f;
+    public float spawnInterval = 2.0f;
+
+    private void Start() {
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
-    void spawnRandomAnimal()
+    void SpawnRandomAnimal()
     {
         Instantiate(
                 animalPrefabs[Random.Range(0, animalPrefabs.Length)],
-                transform.position + new Vector3(Random.Range(-xSpawnRange, xSpawnRange), 0, 0),
+                transform.position + new Vector3(Random.Range(-xSpawnRange, xSpawnRange), 0, zSpawnOffset),
                 transform.rotation
                 );
     }
