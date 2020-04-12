8d58431a0fb13356a233d17bb8f357f2647419f0
Bastian Hodapp <bastianhodapp@gmail.com>
2020-04-12 13:57:11 +0200

    Added ability to spawn animals with s

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+    public float xSpawnRange = 13f;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            spawnRandomAnimal();
+        }
+    }
+
+    void spawnRandomAnimal()
+    {
+        Instantiate(
+                animalPrefabs[Random.Range(0, animalPrefabs.Length)],
+                transform.position + new Vector3(Random.Range(-xSpawnRange, xSpawnRange), 0, 0),
+                transform.rotation
+                );
+    }
+}
