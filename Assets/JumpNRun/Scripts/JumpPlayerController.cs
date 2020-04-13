eb06ae57d6bb7944ec0a6c82359e9ef4e86f68cd
Bastian Hodapp <bastianhodapp@gmail.com>
2020-04-13 18:59:33 +0200

    Activated spawnmanager and made player grounded

--- Assets/JumpNRun/Scripts/JumpPlayerController.cs
+++ Assets/JumpNRun/Scripts/JumpPlayerController.cs
@@ -2,21 +2,29 @@
 
 public class JumpPlayerController : MonoBehaviour
 {
-    public float jumpForce = 1000f;
+    public float jumpForce;
+    public bool isGrounded = true;
+    public float gravityModifier;
 
     private Rigidbody playerRigidbody;
 
     void Start()
     {
         playerRigidbody = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             playerRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
         }
+    }
+
+    private void OnCollisionEnter(Collision other) {
+        isGrounded = true;
     }
 }
