190eb43871be5e3dddfcf21472be2de802e7488a
Bastian Hodapp <bastianhodapp@gmail.com>
2020-04-13 18:26:51 +0200

    Create new character with controller and adding force to jump

---
+++ Assets/JumpNRun/Scripts/JumpPlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class JumpPlayerController : MonoBehaviour
+{
+    public float jumpForce = 1000f;
+
+    private Rigidbody playerRigidbody;
+
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRigidbody.AddForce(Vector3.up * jumpForce);
+        }
+    }
+}
